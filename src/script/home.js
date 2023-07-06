6efcb38829d520dd3eab991a24d64603ce4b510b
KimDoHyeon <ehcjswo1@gmail.com>
2023-07-06 11:45:10 +0900

    07-06

--- src/script/home.js
+++ src/script/home.js
@@ -8,7 +8,7 @@ function pageClick() {
             pageOut();
             setTimeout(() => {
             window.location.href = hrefName;
-            }, 4100);
+            }, 4300);
         });
     });
 }
